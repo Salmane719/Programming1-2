6a3aa5217e366949602aa0b8e87896223cbe3af2
Salmane719 <61871522+Salmane719@users.noreply.github.com>
2021-05-19 19:24:35 +0100

    Add files via upload

---
+++ Programming-1/Q5.c
@@ -0,0 +1,22 @@
+
+#include <stdio.h>
+
+int main(void)
+{
+
+    long double i = 1, n;
+    /*Sets our variables to be able to compute numbers larger than the 50th pell term*/
+    long double sum = 0;
+
+    /*User Input*/
+    printf("Enter how many terms you want: ");
+    scanf("%Lf", &n);
+
+    //Continuously adds our statement
+    for (i; i <= n; i++)
+    {
+        sum += 1 / i;
+    }
+    printf("Your answer is: %Lf", sum);
+    return 0;
+}
---
+++ Programming-1/binary.c
@@ -0,0 +1,29 @@
+
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    int input, i; //Initialize our variables for later use
+
+    /*User Input*/
+    printf("Enter your your decimal number: ");
+    scanf("%d", &input);
+
+    //Gets the number of terms in the binary array goes where it is 2^i & i is the length of the arry
+    i = log(input) / log(2);
+    int arr[i];
+
+    /*Gets the binary numbers and stores it in an array */
+    for (int j = i; j >= 0; j--)
+    {
+        arr[j] = input % 2;
+        input = input / 2;
+    }
+    //Prints out the binary array
+    for (int k = 0; k <= i; k++)
+    {
+        printf("%d", arr[k]);
+    }
+    return 0;
+}
